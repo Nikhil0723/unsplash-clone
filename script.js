b80d6ff41d16d576125d52335393548796e7635e
Nikhil Vishwakarma <138506850+Nikhil0723@users.noreply.github.com>
2023-07-05 21:19:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+const accessKey = "o_G8kWm_Gwjex35T7z8L8cbc0173WyZN3Xx1DF0oyN4";
+const formEL = document.querySelector('form');
+const inputEl = document.getElementById('search-input');
+const searchResults= document.querySelector('.search-results');
+const showMore = document.getElementById('load-more-btn');
+
+let inputData = '';
+let page = 1 ;
+
+  async function searchImage(){
+inputData = inputEl.value
+const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`
+
+ const response = await fetch(url)
+ const data = await response.json()
+
+  console.log(data)
+
+ const results = data.results
+
+   if(page ===1) {
+    searchResults.innerHTML = ''
+ }
+
+ results.map((result)=>{
+    const imageWrapper = document.createElement('div');
+    imageWrapper.classList.add('search-result');
+    const image = document.createElement('img');
+    image.src = result.urls.small ;
+    image.alt = result.alt_description;
+    const imageLink = document.createElement('a');
+    imageLink.href = result.links.html;
+    imageLink.target = '_blank';
+    imageLink.textContent = result.alt_description  ;
+
+    imageWrapper.appendChild(image);
+    imageWrapper.appendChild(imageLink);
+    searchResults.appendChild(imageWrapper);
+ })
+     page++
+
+   if (page > 1) {
+    showMore.style.display ='block'
+   }
+
+}
+
+formEL.addEventListener( 'submit' , (event)=>{
+    event.preventDefault()
+    page = 1
+    searchImage()
+})
+
+showMore.addEventListener( 'click' , (event)=>{
+   searchImage()
+})
